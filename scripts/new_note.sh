874cff49d57ca2801369b05369462dca3286f628
Ram Su <catmorte@gmail.com>
2023-11-27 23:55:47 +0300

    Update new_note.sh

--- scripts/new_note.sh
+++ scripts/new_note.sh
@@ -1,10 +1,16 @@
-# Create a new note with the current time as the title and sync
+# Create a new note with the current time as the title
 new_note() {
 	current_date=$(date "+%Y.%m.%d")
 	current_time=$(date "+%H:%M:%S")
+	echo "Enter note name: "
 	read note_name
 	mkdir -p ~/notes/
 	touch ~/notes/"$current_date"_"$current_time"_"$note_name".md
 	nvim ~/notes/"$current_date"_"$current_time"_"$note_name".md
-	rclone sync --progress ~/notes/ ggld_e:notes
+	if [[ $? -eq 0 ]]; then
+		echo "Syncing notes..."
+		rclone sync --progress ~/notes/ ggld_e:notes
+	else
+		rm ~/notes/"$current_date"_"$current_time"_"$note_name".md
+	fi
 }
