1e4b3773cf4427a8eea3a88e5bfca8d8a052ea77
Ram Su <catmorte@gmail.com>
2023-11-30 21:28:22 +0300

    Update sync_notes.sh

--- scripts/sync_notes.sh
+++ scripts/sync_notes.sh
@@ -1,3 +1,12 @@
 sync_notes() {
 	rclone sync --progress ~/notes/ ggld_e:notes
+	if ! command -v ntfy &>/dev/null; then
+		echo "synced"
+	else
+		ntfy send "Notes synced"
+	fi
 }
+
+if [ "${1}" != "--source-only" ]; then
+	sync_notes "${@}"
+fi
