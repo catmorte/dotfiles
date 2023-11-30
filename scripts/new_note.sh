5cf3582ab90bdd816934f4ea820e6bf799f911e3
Ram Su <catmorte@gmail.com>
2023-11-30 13:25:53 +0300

    Update new_note.sh

--- scripts/new_note.sh
+++ scripts/new_note.sh
@@ -1,16 +1,35 @@
-# Create a new note with the current time as the title
+#!/bin/bash
 new_note() {
+	_zsh_highlight() {}
+	notes=~/notes
+	typeset -a options
+	options=("$notes"/*)
+	iter=1
+	for opt in "${options[@]}"; do
+		options[$iter]=$(basename "$opt")
+		((iter++))
+	done
+	select opt in "${options[@]}"; do
+		if [ "$opt" != "" ]; then
+			break
+		fi
+	done
+
 	current_date=$(date "+%Y.%m.%d")
 	current_time=$(date "+%H:%M:%S")
+
 	echo "Enter note name: "
 	read note_name
-	mkdir -p ~/notes/"$current_date"
-	touch ~/notes/"$current_date"/"$current_time"_"$note_name".md
-	nvim ~/notes/"$current_date"/"$current_time"_"$note_name".md
+
+	folder=$notes/"$opt"/"$current_date"
+	mkdir -p $folder
+	fullapth=$folder/"$current_time"_"$note_name".md
+	touch $fullapth
+	nvim $fullapth
 	if [[ $? -eq 0 ]]; then
 		echo "Syncing notes..."
 		rclone sync --progress ~/notes/ ggld_e:notes
 	else
-		rm ~/notes/"$current_date"_"$current_time"_"$note_name".md
+		rm $fullapth
 	fi
 }
