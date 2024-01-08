cef8d4c889c99b809b96ddb3218c8d84528becb1
Ram Su <catmorte@gmail.com>
2024-01-08 13:30:05 +0300

    Update open_note.sh

--- scripts/open_note.sh
+++ scripts/open_note.sh
@@ -1,28 +1,22 @@
 #!/bin/zsh
 open_note() {
-	_zsh_highlight() {}
-	notes=~/notes
-	typeset -a options
-	options=("$notes"/*)
-	iter=1
-	for opt in "${options[@]}"; do
-		options[$iter]=$(basename "$opt")
-		((iter++))
-	done
-	select opt in "${options[@]}"; do
-		if [ "$opt" != "" ]; then
-			break
-		fi
-	done
+    _zsh_highlight() {}
+    notes=~/notes
+    opt=$(ls $notes | fzf)
+    if [[ "$?" -ne 0 ]]; then
+        return
+    fi
 
-  note_name=$(ls $notes/"$opt" | fzf)
-
-	folder=$notes/"$opt"/"$note_name"
-	mkdir -p $folder
-	fullapth=$folder/note.md
-	nvim $fullapth
+    note_name=$(ls $notes/"$opt" | fzf)
+    if [[ "$?" -ne 0 ]]; then
+        return
+    fi
+    folder=$notes/"$opt"/"$note_name"
+    fullapth=$folder/note.md
+    nvim $fullapth
 }
 
 if [ "${1}" != "--source-only" ]; then
-	open_note "${@}"
+    open_note "${@}"
 fi
+
