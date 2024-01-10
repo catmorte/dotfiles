f4736e8fa16c530afed0579b460a1b3615dd56d1
Ram Su <catmorte@gmail.com>
2024-01-10 16:03:03 +0300

    Update new_note.sh

--- scripts/new_note.sh
+++ scripts/new_note.sh
@@ -1,39 +1,31 @@
 #!/bin/zsh
 new_note() {
-	_zsh_highlight() {}
-	notes=~/notes
-	typeset -a options
-	options=("$notes"/*)
-	iter=1
-	for opt in "${options[@]}"; do
-		options[$iter]=$(basename "$opt")
-		((iter++))
-	done
-	select opt in "${options[@]}"; do
-		if [ "$opt" != "" ]; then
-			break
-		fi
-	done
+    _zsh_highlight() {}
+    notes=~/notes
+    opt=$(ls $notes | fzf)
+    if [[ "$?" -ne 0 ]]; then
+        return
+    fi
 
-	current_date=$(date "+%Y.%m.%d")
-	current_time=$(date "+%H:%M:%S")
+    current_date=$(date "+%Y.%m.%d")
+    current_time=$(date "+%H:%M:%S")
 
-	echo "Enter note name: "
-	read note_name
+    echo "Enter note name: "
+    read note_name
 
-	folder=$notes/"$opt"/"$current_date"_"$current_time"_"$note_name"
-	mkdir -p $folder
-	fullapth=$folder/note.md
-	touch $fullapth
-	nvim $fullapth
-	if [[ $? -eq 0 ]]; then
-		nohup ./sync_notes.sh >/dev/null 2>&1 &
-		disown
-	else
-		rm $fullapth
-	fi
+    folder=$notes/"$opt"/"$current_date"_"$current_time"_"$note_name"
+    mkdir -p $folder
+    fullapth=$folder/note.md
+    touch $fullapth
+    nvim $fullapth
+    if [[ $? -eq 0 ]]; then
+        nohup ./sync_notes.sh >/dev/null 2>&1 &
+        disown
+    else
+        rm $fullapth
+    fi
 }
 
 if [ "${1}" != "--source-only" ]; then
-	new_note "${@}"
+    new_note "${@}"
 fi
