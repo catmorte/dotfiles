91273cc6bf6841f704cdd576ec57e4d809e6b0f4
Ram Su <catmorte@gmail.com>
2024-02-07 14:24:28 +0300

    Update cheats.sh

--- scripts/cheats.sh
+++ scripts/cheats.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
-languages=('c' 'cpp' 'golang' 'python' 'typescript' 'zsh' 'tmux' 'lua' 'bash' 'css' 'html' 'javascript' 'ruby')
-utils=('curl' 'wget' 'vi' 'vim' 'grep' 'awk' 'sed' 'find' 'ls' 'cat' 'tail' 'head' 'tar' 'gzip' 'less' 'git' 'ssh' 'tmux' 'htop' 'top' 'ps' 'kill' 'docker' 'kubectl' 'zip' 'tmux')
-selected=$(printf "%s\n" "${languages[@]}" "${utils[@]}" | fzf)
-if [[ -z $selected ]]; then
+types=('language' 'utility')
+selected_type=$(printf "%s\n" "${types[@]}" | fzf)
+if [[ -z $selected_type ]]; then
     exit 0
 fi
-
-read -p "Enter Query: " query
-
-if [[ ${languages[@]} =~ $selected ]]; then
+if [[ $selected_type == 'language' ]]; then
+    languages=('c' 'cpp' 'golang' 'python' 'typescript' 'zsh' 'tmux' 'lua' 'bash' 'css' 'html' 'javascript' 'ruby')
+    selected=$(printf "%s\n" "${languages[@]}" | fzf)
+    if [[ -z $selected ]]; then
+        exit 0
+    fi
+    read -p "Enter Query: " query
     query=$(echo $query | tr ' ' '+')
     tmux neww bash -c "curl cht.sh/$selected/$query | less"
 else
+    utils=('curl' 'wget' 'vi' 'vim' 'grep' 'awk' 'sed' 'find' 'ls' 'cat' 'tail' 'head' 'tar' 'gzip' 'less' 'git' 'ssh' 'tmux' 'htop' 'top' 'ps' 'kill' 'docker' 'kubectl' 'zip')
+    selected=$(printf "%s\n" "${utils[@]}" | fzf)
+    if [[ -z $selected ]]; then
+        exit 0
+    fi
+    read -p "Enter Query: " query
     tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
 fi
