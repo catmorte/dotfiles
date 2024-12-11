a61c6a70113031161cc1e585ec7d3a7bc5f6e708
catmorte <catmorte@gmail.com>
2024-12-12 00:21:00 +0300

    upd

--- nvim/.config/nvim/lua/plugins/user.lua
+++ nvim/.config/nvim/lua/plugins/user.lua
@@ -118,4 +118,22 @@ return {
   {
     "echasnovski/mini.surround",
   },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-file-browser.nvim",
+      "nvim-lua/plenary.nvim",
+      {
+        "AstroNvim/astrocore",
+        opts = {
+          mappings = {
+            n = {
+              ["<Leader>fe"] = { "<Cmd>Telescope file_browser<CR>", desc = "Open File browser" },
+            },
+          },
+        },
+      },
+    },
+    opts = function() require("telescope").load_extension "file_browser" end,
+  },
 }
